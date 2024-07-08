02b26a78be719842564298e836db709c1e7c3dc5
a13kon-tech <a13kon@yandex.ru>
2024-07-08 19:45:19 +0300

    const added

--- EasyModbusTCP/Program.cs
+++ EasyModbusTCP/Program.cs
@@ -3,39 +3,38 @@ using System.Net;
 
 var client = new ModbusTcpClient();
 bool connect = false;
+const string ip = "172.17.5.103";
+const int port = 502;
 
-Connect(connect);
-
-
+Connect(connect, ip, port);
 
 while (true)
 {
    
-    System.Threading.Thread.Sleep(500);
+    Thread.Sleep(500);
     try
     {
-        var ShortData = client.ReadInputRegisters<float>(0xFF, 4, 1);
-        foreach (var value in ShortData)
+        var floatData = client.ReadInputRegisters<float>(0xFF, 4, 1);
+        foreach (var value in floatData)
         {
             Console.WriteLine(value);
         }
     }
     catch {
         connect = client.IsConnected;
-        Connect(connect);
+        Connect(connect, ip, port);
     } 
 
 }
-    Console.ReadLine();
 
-void Connect(bool connect)
+void Connect(bool connect, string ip, int port)
 {
     while (!connect)
     {
 
         try
         {
-            client.Connect(new IPEndPoint(IPAddress.Parse("172.17.5.103"), 502), ModbusEndianness.BigEndian);
+            client.Connect(new IPEndPoint(IPAddress.Parse(ip), port), ModbusEndianness.BigEndian);
             connect = client.IsConnected;
             Console.WriteLine("Connected");
         }
