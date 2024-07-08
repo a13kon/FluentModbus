dfc9f7aa29f25fdfe14808b2def35516cba21f85
a13kon-tech <a13kon@yandex.ru>
2024-07-08 19:07:13 +0300

    initial commit

---
+++ EasyModbusTCP/Program.cs
@@ -0,0 +1,45 @@
+﻿using FluentModbus;
+using System.Net;
+
+var client = new ModbusTcpClient();
+bool connect = false;
+
+Connect(connect);
+
+
+
+while (true)
+{
+   
+    System.Threading.Thread.Sleep(500);
+    try
+    {
+        var ShortData = client.ReadInputRegisters<float>(0xFF, 4, 1);
+        foreach (var value in ShortData)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    catch {
+        connect = client.IsConnected;
+        Connect(connect);
+    } 
+
+}
+    Console.ReadLine();
+
+void Connect(bool connect)
+{
+    while (!connect)
+    {
+
+        try
+        {
+            client.Connect(new IPEndPoint(IPAddress.Parse("172.17.5.103"), 502), ModbusEndianness.BigEndian);
+            connect = client.IsConnected;
+            Console.WriteLine("Connected");
+        }
+        catch (Exception ex) { Console.WriteLine(ex.Message); }
+    }
+}
+
