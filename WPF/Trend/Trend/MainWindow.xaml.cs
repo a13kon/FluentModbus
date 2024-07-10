b52eeb1f1ec4cbbb1599eba767e8f1abc6223266
a13kon <a13kon@yandex.ru>
2024-07-10 17:26:35 +0300

    trend

--- WPF/Trend/Trend/MainWindow.xaml.cs
+++ WPF/Trend/Trend/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace Trend
 {
@@ -20,13 +21,34 @@ namespace Trend
         public MainWindow()
         {
             InitializeComponent();
+
+            List<double> dataX = new List<double>();
+            List<double> dataY = new List<double>();
+            int counter = 0;
+
             
-        }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Salam");
+
+            DispatcherTimer t = new DispatcherTimer();
+            t.Tick += (o, e) => Click_2(o, e, ref dataX, ref dataY, ref counter);
+            t.Interval = TimeSpan.FromSeconds(1);
+            t.Start();
+
+
         }
 
-      
+        private void Click_2(object o, EventArgs e, ref List<double> dataX, ref List<double> dataY, ref int counter)
+        {
+            Random r = new Random();
+            dataY.Add(r.NextDouble() * 10);
+            dataX.Add(counter++);
+            WpfPlot1.Plot.Add.Scatter(dataX.ToArray(), dataY.ToArray());
+            WpfPlot1.Refresh();
+           // throw new NotImplementedException();
+
+           
+        }
+
+
+
     }
 }
