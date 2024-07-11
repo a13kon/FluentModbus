37ef2655ab9f9f725bf60979aeefd1dc78e51a5e
a13kon-tech <a13kon@yandex.ru>
2024-07-11 12:39:52 +0300

    trend

--- WPF/Trend/Trend/MainWindow.xaml.cs
+++ WPF/Trend/Trend/MainWindow.xaml.cs
@@ -25,39 +25,101 @@ namespace Trend
 
             List<double> dataX = new List<double>();
             List<double> dataY = new List<double>();
-            int counter = 0;
+            List<double> dataZ = new List<double>();
+            double counter = 0.0f;
+            bool start = true;
 
-            ScottPlot.Plot myPlot = new();
+            //Chart.Plot.FigureBackground.Color = ScottPlot.Color.FromHex("#07263b");
+            Chart.Plot.Axes.SetLimits(0, 50, 0, 20);
+            Chart.Plot.Axes.AutoScale();
+            Chart.Plot.ShowLegend();
 
-         
+            
 
             DispatcherTimer t = new DispatcherTimer();
-            t.Tick += (o, e) => Click_2(o, e, ref dataX, ref dataY, ref counter);
-            t.Interval = TimeSpan.FromSeconds(1);
-            t.Start();
+            t.Tick += (o, e) => TimerTick(o, e, ref dataX, ref dataY, ref dataZ, ref counter);
+            t.Interval = TimeSpan.FromMilliseconds(200);
+            //t.Start();
+
+            button_start.Click += (o, e) => Button_Stop(o, e, ref t, ref start);
+            button_clear.Click += (o, e) => Button_Clear(o, e, ref dataX, ref dataY, ref dataZ, ref counter);
+
 
 
         }
 
-        private void Click_2(object o, EventArgs e, ref List<double> dataX, ref List<double> dataY, ref int counter)
+        private void TimerTick(object o, EventArgs e, ref List<double> dataX, ref List<double> dataY, ref List<double> dataZ, ref double counter)
         {
             Random r = new Random();
-            dataY.Add(r.NextDouble() * 10 * counter);
-            dataX.Add(counter++);
-            var sig = Chart.Plot.Add.ScatterLine(dataX.ToArray(), dataY.ToArray());
 
-            sig.LineColor = ScottPlot.Color.FromHex("#a0acb5");
+            dataY.Add(r.NextDouble());
+            dataX.Add(counter+=3);
+            dataZ.Add(r.NextDouble() + 2);
 
-            Chart.Plot.FigureBackground.Color = ScottPlot.Color.FromHex("#07263b");
-            
-            Chart.Plot.SavePng("demo.png", 400, 300);
+            //Chart.Plot.Axes.SetLimits(0, 5000, 0, 20);
+            //Chart.Plot.Axes.AutoScale();
+
+            Chart.Plot.Clear();
+
+           
+
+
+            var sig = Chart.Plot.Add.ScatterLine(dataX.ToArray(), dataY.ToArray(), ScottPlot.Color.FromHex("#000000"));
+            sig.LegendText = "Preassure";
+
+            var sig2 = Chart.Plot.Add.ScatterLine(dataX.ToArray() ,dataZ.ToArray());
+            sig2.LegendText = "Temperature";
+
+            //sig.LineColor = ScottPlot.Color.FromHex("#000000");
+
+
+            //Chart.Plot.SavePng("demo.png", 400, 300);
             Chart.Refresh();
            // throw new NotImplementedException();
 
            
         }
 
+        private void Button_Stop(object sender, RoutedEventArgs e, ref DispatcherTimer t, ref bool start)
+        {
+
+            if (start)
+            {
+                t.Start();
+                button_start.Content = "Stop";
+            }
+            else
+            {
+                t.Stop();
+                button_start.Content = "Start";
+            }
+
+            start = !start;
+                //Chart.Plot.Axes.SetLimits(0, 5000, 0, 20);
+                //Chart.Plot.Axes.AutoScale();
+                
 
 
+        }
+        private void Button_Clear(object sender, RoutedEventArgs e, ref List<double> dataX, ref List<double> dataY, ref List<double> dataZ, ref double counter)
+        {
+            counter = 0.0f;
+            dataX.Clear();
+            dataY.Clear();
+            dataZ.Clear();
+            Chart.Plot.Clear();
+            Chart.Refresh();
+
+
+        }
+
+ 
+
+        private void Button_Follow(object sender, RoutedEventArgs e)
+        {
+
+            Chart.Plot.Axes.SetLimits(0, 5000, 0, 20);
+            Chart.Plot.Axes.AutoScale();
+        }
     }
 }
